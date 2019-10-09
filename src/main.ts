2c7f94ad0ccad71c0b11f7dc5d340eb357e018fb
David Anthoff <anthoff@berkeley.edu>
2019-10-09 09:56:26 -0700

    Don't fail on error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function run() {
     try {
         await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
     } catch (error) {
-        core.setFailed(error.message)
+        // We are making errors non fatal for now because this for example
+        // will generally not work if triggered by a PR, which is a common
+        // situation.
+        // core.setFailed(error.message)
     }
 }
 
