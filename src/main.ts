91bd8645e748a04754d48e39454e58794e9709fd
David Anthoff <anthoff@berkeley.edu>
2019-10-09 09:56:08 -0700

    Use tagged version of Coverage.jl

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import * as exec from '@actions/exec'
 
 async function run() {
     try {
-        // await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
-        await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add(PackageSpec(name="Coverage", rev="master")); using Coverage; Codecov.submit(process_folder())'])
+        await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
     } catch (error) {
         core.setFailed(error.message)
     }
